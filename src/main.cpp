97c5c47f95233c67da63dda5ec7bf1cdbbe522f9
Mathieu BARLEON <mathieu.barleon@epitech.eu>
2026-03-02 14:12:09 +0100

    feat: base logger

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,75 @@
 
 #include <Objects3D/OMesh.hpp>
 #include <Renderer/RaylibRenderer.hpp>
+#include <Splinter/Utils/Logger.hpp>
+#include <cstdarg>
+#include <cstdio>
 #include <iostream>
+#include <raylib.h>
+#include <string>
+
+static void RaylibToLogger([[maybe_unused]] int         logLevel,
+                           [[maybe_unused]] const char* text, [[maybe_unused]] va_list args)
+{
+#if !defined(SPLINTER_DEBUG)
+    return;
+#endif
+
+    char* buf = NULL;
+
+#if defined(__clang__)
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wformat-nonliteral"
+#elif defined(__GNUC__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wformat-nonliteral"
+#endif
+
+    int ret = vasprintf(&buf, text, args);
+
+#if defined(__clang__)
+#pragma clang diagnostic pop
+#elif defined(__GNUC__)
+#pragma GCC diagnostic pop
+#endif
+
+    if (ret < 0)
+    {
+        return;
+    }
+
+    const char* lvl = "INFO";
+    switch (logLevel)
+    {
+        case LOG_TRACE:
+            lvl = "TRACE";
+            break;
+        case LOG_DEBUG:
+            lvl = "DEBUG";
+            break;
+        case LOG_INFO:
+            lvl = "INFO";
+            break;
+        case LOG_WARNING:
+            lvl = "WARN";
+            break;
+        case LOG_ERROR:
+            lvl = "ERROR";
+            break;
+        case LOG_FATAL:
+            lvl = "FATAL";
+            break;
+        default:
+            break;
+    }
+
+    splinter::utils::clog("[raylib ", lvl, "] ", buf);
+    std::free(buf);
+}
 
 int main()
 {
+    SetTraceLogCallback(RaylibToLogger);
     renderer::Config         cfg{1280, 720, "Prototype 3D Slicer", 60};
     renderer::RaylibRenderer renderer(cfg);
 
