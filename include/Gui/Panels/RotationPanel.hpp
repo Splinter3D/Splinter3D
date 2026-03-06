b2247b22ee4fb292011515714d13b82ce7bb4225
Kaiwinta <titouan.pradoura@epitech.eu>
2026-03-06 14:43:12 +0100

    fix: rotation panel name

--- include/Gui/Panels/RotationPanel.hpp
+++ include/Gui/Panels/RotationPanel.hpp
@@ -26,7 +26,7 @@ namespace gui::panels
             const renderer::Color kAxisY{67, 160, 71, 255};
             const renderer::Color kAxisZ{30, 136, 229, 255};
 
-            r.drawText(px + kPad, py + kPad, "Transform", 16, renderer::Layer::Overlay);
+            r.drawText(px + kPad, py + kPad, "Rotation", 16, renderer::Layer::Overlay);
 
             float fy = py + kPad + 24.0f;
 
