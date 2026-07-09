c7c4b86f467c501a8a42856054369efcb857c5c7
Mathieu BARLEON <mathieu.barleon@epitech.eu>
2026-07-09 08:29:07 +0200

    fix: ci compilation

--- cmake/4-External.cmake
+++ cmake/4-External.cmake
@@ -1,15 +1,38 @@
 #######################################
 
-find_package(glfw3 CONFIG REQUIRED)
 find_package(raylib CONFIG REQUIRED)
 find_package(nfd CONFIG REQUIRED)
+
+set(GLFW_LINK_TARGET "")
+
+find_package(glfw3 CONFIG QUIET)
+if(TARGET glfw)
+    set(GLFW_LINK_TARGET glfw)
+elseif(TARGET glfw3)
+    set(GLFW_LINK_TARGET glfw3)
+elseif(TARGET glfw::glfw)
+    set(GLFW_LINK_TARGET glfw::glfw)
+else()
+    find_package(PkgConfig QUIET)
+    if(PkgConfig_FOUND)
+        pkg_check_modules(GLFW3 QUIET IMPORTED_TARGET glfw3)
+        if(TARGET PkgConfig::GLFW3)
+            set(GLFW_LINK_TARGET PkgConfig::GLFW3)
+        endif()
+    endif()
+
+    if(NOT GLFW_LINK_TARGET)
+        find_library(GLFW_LIBRARY NAMES glfw glfw3 REQUIRED)
+        set(GLFW_LINK_TARGET "${GLFW_LIBRARY}")
+    endif()
+endif()
 
 # fix: removes duplicate link libraries for raylib when using vcpkg
 if(WIN32 AND TARGET raylib)
     set_property(TARGET raylib PROPERTY INTERFACE_LINK_LIBRARIES "")
 endif()
 
-set(THIRD_PARTY_LIBS raylib glfw nfd::nfd)
+set(THIRD_PARTY_LIBS raylib ${GLFW_LINK_TARGET} nfd::nfd)
 
 if(WIN32)
     # Add Windows-specific libraries for nativefiledialog-extended
--- scripts/Build/Actions/Build.py
+++ scripts/Build/Actions/Build.py
@@ -3,7 +3,9 @@ import pathlib
 
 from ParseArgs import args
 from ConfigureBuild.Cache import ensure_compatible_build_cache
-from ConfigureBuild.BuildSystem import run_build_tool
+from ConfigureBuild.BuildSystem import run_build_tool, _detect_build_system_from_cache, BuildSystem
+from ConfigureBuild.MSVC import ensure_msvc_environment
+from Platform import Platform, get_platform
 
 __all__ = ["build_existing_target"]
 
@@ -16,6 +18,9 @@ def build_existing_target(target: str):
     cwd = os.getcwd()
     try:
         os.chdir(build_dir)
+        build_system = _detect_build_system_from_cache(pathlib.Path.cwd())
+        if get_platform() == Platform.WINDOWS and build_system == BuildSystem.NINJA:
+            ensure_msvc_environment()
         build_type = "Debug" if args.debug_build or args.debug or target == "splinter3D_unit_tests" else "Release"
         run_build_tool(None, target, build_type)
     finally:
